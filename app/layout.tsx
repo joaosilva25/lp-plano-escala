bddc782e39dca2afd07b4d0e1acd1f1f8f16e32c
João Vitor <joaosilva2505@outlook.com>
2026-06-30 20:36:43 -0300

    fix: adjust pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '171392974957474');
+              fbq('init', '253928609695585');
               fbq('track', 'PageView');
             `,
           }}
@@ -50,7 +50,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=171392974957474&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=253928609695585&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
